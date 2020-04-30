ce4982044159007f9cb1d06c46dda2c957670331
Aaron Wentt <aaron.wentt@gmail.com>
2020-04-30 12:55:20 -0400

    adding better styles and svgs

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
-import './css/styles.css';
 import { format } from 'date-fns';
+import './css/styles.css';
+import ClockIcon from './svgs/clock.svg';
+import CalendarIcon from './svgs/calendar.svg';
 
 const getToday = () => (
     format(new Date(), 'EEEE, MMMM do yyyy G')
@@ -11,8 +13,14 @@ const getTheTime = () => (
 
 const getMarkup = (todaysDate, time) => `
     <h1>Yo!</h1>
-    <p>Today's date is <br /> ${todaysDate}</p>
-    <p>The current time is <br />${time}</p>
+    <p>
+        <span class="ico__calendar">${CalendarIcon}</span>
+        Today's date is <strong>${todaysDate}</strong>
+    </p>
+    <p>
+        <span class="ico__clock">${ClockIcon}</span>
+        The current time is <strong>${time}</strong>
+    </p>
 `;
 
 const setMarkup = (markup) => {
