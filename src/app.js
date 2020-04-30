310c83f9cb854de0bbbcc16adc11a73a43ad3e96
Aaron Wentt <aaron.wentt@gmail.com>
2020-04-30 12:32:58 -0400

    finished state for loaders

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
-alert('Hello Browser Guild');
+import './css/styles.css';
+import { format } from 'date-fns';
+
+const getToday = () => (
+    format(new Date(), 'EEEE, MMMM do yyyy G')
+);
+
+const getTheTime = () => (
+    `${format(new Date(), 'hh:mm:ss aaaaa')}m`
+);
+
+const getMarkup = (todaysDate, time) => `
+    <h1>Yo!</h1>
+    <p>Today's date is <br /> ${todaysDate}</p>
+    <p>The current time is <br />${time}</p>
+`;
+
+const setMarkup = (markup) => {
+    document.getElementById('app').innerHTML = markup;
+};
+
+setInterval(() => (
+    setMarkup(getMarkup(getToday(), getTheTime()))
+), 1000);
+
+setMarkup(getMarkup(getToday(), getTheTime()))
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/app.js', // can be an object | string | array
+    entry: {
+        app: './src/app.js',
+    }, // can be an object | string | array
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js',
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 };
