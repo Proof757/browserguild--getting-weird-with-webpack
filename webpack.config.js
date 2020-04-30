c172d07ec7f9a370efdaf25f6846bc7868a40864
Aaron Wentt <aaron.wentt@gmail.com>
2020-04-30 12:19:31 -0400

    finished state for output

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+const path = require('path');
+
 module.exports = {
     entry: './src/app.js', // can be an object | string | array
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+    }
 };
